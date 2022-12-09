8e87be421d124cddee6bf287ba49662686331f28
xyndos <pejuangnode@gmail.com>
2022-12-10 00:31:50 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-295f757fc5756cdb.js
+++ _next/static/chunks/webpack-295f757fc5756cdb.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "7448535faa19e74a" + ".css";
+/******/ 			return "static/css/" + "747bbe42584e3031" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	

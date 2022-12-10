ec90bee4f56b0444842aca5f13f90e8fc74763c4
waxnoxing <waxnoxing@gmail.com>
2022-12-10 09:46:30 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-3e4c3404777f7970.js
+++ _next/static/chunks/webpack-3e4c3404777f7970.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "669ce704328b86b2" + ".css";
+/******/ 			return "static/css/" + "a3fb4808dcfbcd26" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
